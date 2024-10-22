1a499560b9995850364fdc9112ae5699c5f761ae
Lavanya <lavanya@Lavanyas-MacBook-Pro.local>
2024-10-22 12:52:50 -0700

    initial commit -- left and right shift operators

---
+++ src/com/practise/ShiftOperators.java
@@ -0,0 +1,11 @@
+package com.practise;
+
+public class ShiftOperators {
+    public static void main(String[] args) {
+        System.out.println("********* right shift operator means divide by 2 ********");
+        System.out.printf("256 >> 2: %1$d", 256>>2);
+
+        System.out.println("********* left shift operator means multiply by 2 ********");
+        System.out.printf("256 << 2: %1$d", 256<<2);
+    }
+}
